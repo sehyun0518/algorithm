f70b0153bee4ee257016d2ce0917da4e9fffd3e6
SeHyun <31209436+sehyun0518@users.noreply.github.com>
2025-09-11 19:55:13 +0900

    [level 2] Title: 전화번호 목록, Time: 132.43 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,7 @@
+function solution(phone_book) {
+    let arr = phone_book.sort();
+    for(let i = 0; i < phone_book.length - 1; i++){
+        if(arr[i + 1].startsWith(arr[i])) return false;
+    }
+    return true;
+}
