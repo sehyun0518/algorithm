d5989a7c7a19210af80ba785d8afeb286b28f652
SeHyun <31209436+sehyun0518@users.noreply.github.com>
2026-02-26 20:47:06 +0900

    [level 3] Title: 거스름돈, Time: 18.50 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.js
@@ -0,0 +1,11 @@
+function solution(n, money) {
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    
+    for (const m of money) {
+        for (let i = m; i <= n; i++) {
+            dp[i] += dp[i - m];
+        }
+    }
+    return dp[n];
+}
